46fa87aa45ffd9827cf582dade614aae6990566f
zhy <2431772687@qq.com>
2018-10-17 09:34:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <home></home>
+  </div>
+</template>
+
+<script>
+  import home from "./components/home.vue"
+export default {
+      data(){
+        return {
+
+        }
+      },
+      components:{
+        home
+      }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		home
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data(){
+			return {
+
+			}
+		}
+
+	}
+</script>
+<style type="text/css">
+
+</style>
